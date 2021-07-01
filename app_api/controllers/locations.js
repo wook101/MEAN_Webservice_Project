0325facdad108830f8499c8b5e4c16f4a5518ec2
wook101 <ehddnr1022@gmail.com>
2021-07-01 21:01:32 +0900

    /:locationid api get요청에 대한 에러 처리하기

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -18,13 +18,24 @@ module.exports.locationsCreate = function(req,res){
     
 };
 module.exports.locationsReadOne = function(req,res){
-    let locationid = req.params.locationid;
-    Location
-        .findById(locationid)
-        .exec(function(err, location){  //db쿼리 실행이 비동기적으로 작동, node메인 프로세스 블록킹을 방지
-            console.log("findById complete");
-            sendJsonResponse(res, 200, location);
-        });
+    
+    if (req.params && req.params.locationid){
+        Location
+            .findById(req.params.locationid)
+            .exec(function(err, location){  //db쿼리 실행이 비동기적으로 작동, node메인 프로세스 블록킹을 방지
+                if(!location){
+                    sendJsonResponse(res, 404, {"message":"mongodb에 해당 locationid가 존재하지 않습니다."});    
+                    return;
+                }
+                else if(err){
+                    sendJsonResponse(res, 404, err);   //mongoose가 실행에러를 리턴할때    
+                    return;
+                }
+                sendJsonResponse(res, 200, location);   //응답 성공
+            });
+    }else{  //실패한 요청에 대한 404에러,
+        sendJsonResponse(res, 404, {"message":"요청내에 locationid 존재하지 않습니다."}); 
+    }
 
 };
 module.exports.locationsUpdateOne = function(req,res){
