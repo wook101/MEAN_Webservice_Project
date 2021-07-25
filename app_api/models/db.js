585856e502a6d62ca9d73e51149a76e483b6b620
wook101 <ehddnr1022@gmail.com>
2021-07-25 19:18:41 +0900

    heroku와 db mongodb atlas 연결 테스트4

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -6,7 +6,7 @@ const dbURI = 'mongodb://localhost/meanWook';
 if (process.env.NODE_ENV === 'production'){
     //dbURI = "mongodb+srv://wook:abc1234!@cluster0.ivwng.mongodb.net/meanWook?retryWrites=true&w=majority";
     const { MongoClient } = require('mongodb');
-    const uri = "mongodb+srv://wook:abc1234!@cluster0.ivwng.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+    const uri = "mongodb+srv://wook:abc1234!@cluster0.ivwng.mongodb.net/meanWook?retryWrites=true&w=majority";
     const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
     client.connect(err => {
     const collection = client.db("test").collection("devices");
